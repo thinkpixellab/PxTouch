5fd705f8a5738a9ba952a92fd2c2f75ce4c6d3ed
Joel Fillmore <joelfillmore@gmail.com>
2016-04-28 16:47:37 -0700

    Update grunt packages

--- gruntfile.js
+++ gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function(grunt) {
                 curly: true,
                 eqeqeq: true,
                 immed: true,
-                latedef: true,
+                latedef: 'nofunc',
                 newcap: true,
                 noarg: true,
                 sub: true,
